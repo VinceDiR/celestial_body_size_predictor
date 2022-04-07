4f86ce07b7a6c0c6fff27290bb14a16ed172bc0e
Nate <88605064+NateDiR@users.noreply.github.com>
2022-04-07 00:32:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,10 @@ import xgboost as xgb
 import matplotlib.pyplot as plt
 from google.oauth2 import service_account
 
+credentials = st.secrets['gcp_service_account']
+
 credentials = service_account.Credentials.from_service_account_info(
-    **st.secrets['gcp_service_account'], scopes=["https://www.googleapis.com/auth/cloud-platform"])
+    credentials, scopes=["https://www.googleapis.com/auth/cloud-platform"])
 
 st.image('images/known_asteroids.gif')
 
