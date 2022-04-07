e1c02583d2d33fa8e967380090bfb63f8b274aa4
Nate <88605064+NateDiR@users.noreply.github.com>
2022-04-06 23:13:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
-from numpy import False_
 import pandas as pd
 import streamlit as st
 import xgboost as xgb
 import matplotlib.pyplot as plt
-from PIL import Image
+from google.oauth2 import service_account
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp-service-account"]
+)
 
 st.image('images/known_asteroids.gif')
 
@@ -16,7 +19,7 @@ How well can we predict the size of an asteroid with Gradient Boosted Regression
 bucket_name="ndir-metis-bucket"
 path=f"gs://{bucket_name}/asteroid/processed_asteroid_data.csv"
 data = pd.read_csv(path,
-                    storage_options={'token' : 'secrets.toml'},
+                    storage_options={'token' : credentials},
                     low_memory=False,
                     index_col=0)
 
